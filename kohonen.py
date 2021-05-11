b70dfb9c8853b7009ec18a1e87cfc3ca4636d925
Julian Tallar <jtallar@itba.edu.ar>
2021-05-11 15:32:30 -0300

    Added U matrix + some bug fixes

--- kohonen.py
+++ kohonen.py
@@ -3,6 +3,7 @@ import random
 import math
 import numpy as np
 import perceptron as p
+import utils
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
@@ -12,7 +13,7 @@ def scaled_data(filepath: str):
     # read data_file csv
     full_data = pd.read_csv(filepath)
     # retrieve numeric values, scale data with media and normalize
-    return StandardScaler().fit_transform(full_data.iloc[:, 1:].values)
+    return full_data.iloc[:, 0].values, StandardScaler().fit_transform(full_data.iloc[:, 1:].values)
 
 
 # w0 for neuron initialization with dataset
@@ -22,7 +23,7 @@ def w0_data_init(data: np.ndarray) -> np.ndarray:
 
 # w0 for neuron initialization without dataset
 def w0_random_init(data: np.ndarray) -> np.ndarray:
-    return np.random.uniform(-1, 1, len(data[0]))
+    return np.random.uniform(0, 1, len(data[0]))
 
 
 # accumulation function for oja algorithm
@@ -46,15 +47,18 @@ def init_kohonen_grid(random_init: bool, data: np.ndarray) -> [[p.SimplePerceptr
         grid.append(row)
     return grid
 
+def eucledian_distance(vec_1: np.ndarray, vec_2: np.ndarray) -> float:
+    return np.sqrt(np.sum((vec_1 - vec_2) ** 2))
 
-# gets the indexes of the most similar neuron given the data
+# TODO: Do same thing with correlation distance
+# gets the indexes of the most similar neuron given the data using eucledian distance
 def get_best_neuron_indexes(grid: [[p.SimplePerceptron]], data: np.ndarray) -> (int, int):
     # get the best neuron position
     best_val: float = np.inf
     best_neuron: tuple = (0, 0)
     for i, row in enumerate(grid):
         for j, n in enumerate(row):
-            curr_val: float = np.sqrt(np.sum((data - n.get_normalized_w()) ** 2))
+            curr_val: float = eucledian_distance(data, n.get_w())
             if curr_val < best_val:
                 best_val = curr_val
                 best_neuron = (i, j)
@@ -87,9 +91,10 @@ k: int = config["kohonen"]["k"]
 c: float = config["kohonen"]["c"]
 is_w0_random_init: bool = config["kohonen"]["w0_random_init"]
 iterations: int = config["kohonen"]["kxk_iterations"] * k * k
+plot_boolean: bool = config["plot"]
 
 # retrieve scaled data
-data_scaled = scaled_data(data_filepath)
+countries, data_scaled = scaled_data(data_filepath)
 
 # initialize grid
 kohonen_grid: [[p.SimplePerceptron]] = init_kohonen_grid(is_w0_random_init, data_scaled)
@@ -113,19 +118,55 @@ for iteration in range(0, iterations):
         for (x, y) in pos_neighbours:
             kohonen_grid[x][y].train(_data, eta)
 
-    # update all the w values as the epoch finished
+    # TODO: Check si funca mejor con esto asi o por epoca
+    # update all the w values as the iteration finished
     update_all_w(kohonen_grid)
 
 # finished training
 
 
 # matrix of matches count
+country_list_grid: [[str]] = [[[] for x in range(k)] for x in range(k)]
 registry_count_grid: [[int]] = np.zeros((k, k), dtype=int)
-for _data in data_scaled:
+for country, _data in zip(countries, data_scaled):
     # get the best neuron position
     pos_best: (int, int) = get_best_neuron_indexes(kohonen_grid, _data)
 
     # update index in the corresponding position
     registry_count_grid[pos_best[0]][pos_best[1]] += 1
 
-# TODO matrix u calculation
+    # add country name to list
+    country_list_grid[pos_best[0]][pos_best[1]].append(country)
+
+print('Match matrix countries: ')
+print('\n'.join('{}: {}'.format(*k) for k in enumerate(country_list_grid)))
+
+# TODO: Check if U matrix uses radius=1 (or final) for node neighbours
+# U matrix 
+u_matrix_grid: [[int]] = np.zeros((k, k), dtype=float)
+for i, row in enumerate(kohonen_grid):
+    for j, n in enumerate(row):
+        # get neighbours index list
+        neighbours: [(int, int)] = get_neighbour_neuron_indexes(k, (i, j), r)
+
+        # iterate over neighbours to accumulate eucledian distance
+        sum_dist: float = 0
+        for (x, y) in neighbours:
+            sum_dist += eucledian_distance(n.get_w(), kohonen_grid[x][y].get_w())
+        
+        # save average eucledian distance with neighbours
+        u_matrix_grid[i][j] = sum_dist / len(neighbours)
+       
+# if plot_boolean is true, generate plots
+if plot_boolean:
+    # Plots
+    utils.init_plotter()
+
+    # Plot registry count matrix
+    utils.plot_matrix(registry_count_grid)
+
+    # Plot U Matrix
+    utils.plot_matrix(u_matrix_grid, 'gray', not_exp=True)
+
+    utils.hold_execution()
+
